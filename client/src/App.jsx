1236b8894fbc53dc1a0dd4f0e2b7371a224a9f7d
Sandipan03 <sandipan2018@yahoo.com>
2023-12-20 22:58:36 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,7 +35,6 @@ function App() {
         <p>Upload and share the download link.</p>
         <button onClick={onUploadClick}>Upload</button>
         <input type="file" ref={fileInputRef} onChange={(e)=>{setFile(e.target.files[0])}} style={{display:'none'}}/>
-        <p>Use the link below to download the file:</p>
         <a href={result} target="_blank">{result}</a>
       </div>
     </div>
